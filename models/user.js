51eb3bb6b86462f78be082ec2aae58e4a05c726b
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-06-29 19:46:57 -0700

    fixed error on email validation

--- models/user.js
+++ models/user.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
+
 const userSchema = new Schema({
   username: {
     type: String,
