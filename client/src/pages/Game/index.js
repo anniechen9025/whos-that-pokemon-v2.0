552c5e26ded0aadde22c81effcd4d2300edf97c2
LiHua Anderson <lihua.anderson@gmail.com>
2021-06-30 20:34:41 -0700

    began adding code to Game>index.js

--- client/src/pages/Game/index.js
+++ client/src/pages/Game/index.js
@@ -2,9 +2,39 @@ import React, { useEffect, useState } from "react";
 
 
 function Game() {
+
     return (
         <div>
-            <p>Game</p>
+    <header>
+      <div class="large-font">Who's That Pokemon!</div>
+    </header>
+
+    <main>
+      <section>
+        <div class="card word-guess">
+          <div class="pokemonPic"></div>
+          <div class="large-font word-blanks">_ _ _ _ _ _ _</div>
+        </div>
+        <button class="start-button">Start</button>
+      </section>
+
+      <section>
+        <div class="card results">
+          <div class="win-loss-container">
+            <div>
+              <h2>Pokemon Caught: <span class="win">0 </span> </h2>
+            </div>
+          </div>
+          <div class="card timer">
+            <div class="timer-text">
+              <div class="large-font timer-count">30</div>
+              <h3>seconds remaining</h3>
+            </div>
+
+          </div>
+        </div>
+      </section>
+    </main>
         </div>
     )
 }
