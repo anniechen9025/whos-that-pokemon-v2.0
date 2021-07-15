5add3eac719fe96fbcf39f9088be7fbb22e20789
LiHua Anderson <lihua.anderson@gmail.com>
2021-07-14 20:27:57 -0700

    pushing current code for pages>Game>hooks.js to try and get api call to post user pokemon in DB

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,29 +1,26 @@
 import React, { useEffect, useState } from 'react';
-Christine
 import Button from '../../components/Button';
-import './style.css'
+import './style.css';
 
 function Main() {
   return (
-      <div class="btn-group">
-        <div class="btn ball">
-          <Button className="poke" text="Challenge">
-            <div class="pokemon-ball"></div>
-            <a>
-              Challenge<span data-letters="GO!"></span>
-              <span data-letters="GO!"></span>
-            </a>
-          </Button>
-        </div>
+    <div class="btn-group">
+      <div class="btn ball">
+        <Button className="poke" text="Challenge">
+          <div class="pokemon-ball"></div>
+          <a>
+            Challenge<span data-letters="GO!"></span>
+            <span data-letters="GO!"></span>
+          </a>
+        </Button>
       </div>
-
+    </div>
 
     // <Container>
     // <LeftContainer>
     // <img scr={pokemonLogo} alt="pokemon logo" />
     // </LeftContainer>
     // </Container>
-main
   );
 }
 
