1ea3bab3d8f9376292e6e074da7b06b310671e84
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-14 18:23:27 -0700

    Styles for main

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,12 +1,28 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect, useState } from 'react';
+import Button from '../../components/Button';
+import './style.css'
 
 function Main() {
-    return (
-        <div>
-            <p>Main</p>
+  return (
+      <div class="btn-group">
+        <div class="btn ball">
+          <Button className="poke" text="Challenge">
+            <div class="pokemon-ball"></div>
+            <a>
+              Challenge<span data-letters="GO!"></span>
+              <span data-letters="GO!"></span>
+            </a>
+          </Button>
         </div>
-    )
+      </div>
+
+
+    // <Container>
+    // <LeftContainer>
+    // <img scr={pokemonLogo} alt="pokemon logo" />
+    // </LeftContainer>
+    // </Container>
+  );
 }
 
 export default Main;
