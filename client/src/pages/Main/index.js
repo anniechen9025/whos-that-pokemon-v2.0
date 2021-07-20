b400505e864d39c1be201a8a442c704c4ee86dff
Christine Nguyen <ctine.ngyn@gmail.com>
2021-07-19 19:19:45 -0700

    changes in main

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,40 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import Button from '../../components/Button';
+import React from 'react';
+import { Jumbotron, Button } from 'reactstrap';
 import './style.css';
 
 function Main() {
   return (
-    <div class="btn-group">
-      <div class="btn ball">
-        <Button className="poke" text="Challenge">
-          <div class="pokemon-ball"></div>
-          <a>
-            Challenge<span data-letters="GO!"></span>
-            <span data-letters="GO!"></span>
-          </a>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8a974e3e2557625848883fc82a6bb2098ae60841
-        </Button>
-      </div>
-    </div>
 
-    // <Container>
-<<<<<<< HEAD
-=======
-        </Button>{/* // <Container>
->>>>>>> fdbafb9d16522ef10b9503bf7dd7a844920552da
-=======
->>>>>>> 8a974e3e2557625848883fc82a6bb2098ae60841
-    // <LeftContainer>
-    // <img scr={pokemonLogo} alt="pokemon logo" />
-    // </LeftContainer>
-    // </Container> */}
-      </div>
+    <div>
+      <Jumbotron>
+        <h1 className="display-3">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-2" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p className="lead">
+          <Button color="primary">Learn More</Button>
+        </p>
+      </Jumbotron>
     </div>
-
-  );
+);
 }
 
 export default Main;
