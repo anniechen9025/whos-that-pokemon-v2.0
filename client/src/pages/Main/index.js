271f82406cc7411556d2132d9566094c181c5999
Jouan <77906989+anniechen9025@users.noreply.github.com>
2021-07-21 16:37:15 -0700

    Finalizing

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,26 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import Button from '../../components/Button';
+import { Jumbotron, Button } from 'reactstrap';
+import Buttons from '../../components/Button';
 import './style.css';
 
 function Main() {
   return (
     <div class="btn-group">
-      <div class="btn ball">
-        <Button className="poke" text="Challenge">
+      {/* <div class="btn ball">
+        <Buttons className="poke" text="Challenge">
           <div class="pokemon-ball"></div>
           <a>
             Challenge<span data-letters="GO!"></span>
             <span data-letters="GO!"></span>
           </a>
-        </Button>
-      </div>
+        </Buttons>
+      </div> */}
+      <div>
+      <Jumbotron id = "main">
+        <h1 className="display-3">Hello, Trainer!</h1>
+        <p className="lead">This is a simple guideline to Who's That Pokemon.</p>
+        <hr className="my-2" />
+        <p>Navigate to different pages using the Navigation bar above.</p>
+        <p>On the Game Page use your keyboard to guess all the pokemon from Generation One!</p>
+        <p>Navigate to the Pokedex to view all your Pokemon or restart your game by releasing your pokemon.</p>
+        <p>Connect with other users with our Chatbox feature!</p>
+        <p>Or View your Trainer Rank or update your password at your Profile Page!</p>
+        <br></br>
+        <p className="lead">
+          <Button href="/fun">Learn about Fun Fact!!</Button>
+        </p>
+      </Jumbotron>
     </div>
-
-    // <Container>
-    // <LeftContainer>
-    // <img scr={pokemonLogo} alt="pokemon logo" />
-    // </LeftContainer>
-    // </Container> */}
+    </div>
   );
 }
 
